6116499c937007708c9e8184c5785ce9d8f869dd
nguyentuannguyenan <nguyenan29072001@gmail.com>
2025-09-08 09:35:57 +0200

    fixed eveything

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
       </div>
       <div className="flex gap-3">
         {mySocials.map((social, index) => (
-          <a href={social.href} key={index}>
+          <a href={social.href} key={index} target="_blank" rel="noopener noreferrer">
             <img src={social.icon} className="w-5 h-5" alt={social.name} />
           </a>
         ))}
